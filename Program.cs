bd1e7153ec492995ce0240a88f0d142336f56fbe
Supasiri1062 <122951951+Supasiri1062@users.noreply.github.com>
2023-05-20 19:08:56 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿class Program{
+    static void Main(string[] args){
+        bool continueProgram = true;
+        while(continueProgram){
+            Console.Write("input number: ");
+            int numRows = int.Parse(Console.ReadLine());
+            if(numRows < 0){
+                Console.WriteLine("Invalid Pascal's triangle row number.");
+                continue;
+            } else {
+                for (int i = 0; i <= numRows; i++){
+                    for (int j = 0; j <= i; j++){
+                        Console.Write(CalculatePascalValue(i, j) + " ");
+                    }
+                    Console.WriteLine();
+                }
+                continueProgram = false;
+            }
+           
+        }
+    }
+    static int CalculatePascalValue(int row, int column){
+        if(column == 0 || column == row){
+            return 1;
+        } else {
+            return CalculatePascalValue(row - 1, column - 1) + CalculatePascalValue(row - 1, column);
+        }
+    }
+}
